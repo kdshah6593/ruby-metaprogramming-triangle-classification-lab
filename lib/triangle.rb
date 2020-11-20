c635d0364d940ce4d027eaf108eaff2d9acd05e1
Kunal <kdshah6593@gmail.com>
2020-11-20 16:55:48 -0500

    commit tree 67b801dda180b1bdd7ca42ef82d00cd5a91efa9f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if @a <= 0 && @b <= 0 && @c <= 0 #checks if sides > 0
+      raise TriangleError
+    elsif @a + @b <= @c || @a + @c <= @b || @b + @c <= @a #checks triangle inequality theorem
+      raise TriangleError
+    else
+      if @a == @b && @a == @c && @b == @c #checks if equilateral
+        :equilateral
+      elsif @a == @b && @a != @c || @a == @c && @a != @b || @b == @c && @c != @a #checks if isoceles
+        :isosceles
+      elsif @a != @b && @a != @c && @b != @c #checks if scalene
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
